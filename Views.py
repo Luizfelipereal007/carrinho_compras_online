08638754e0e7783f7f69686cd91b9ae74bc411b9
l2fm <l2fm2005@gmail.com>
2024-08-08 21:40:48 -0400

    views1

--- Views.py
+++ Views.py
@@ -1,11 +1,35 @@
 from Function import*
 
-def fluxo_principal():
+def menu_principal():
     print("1-Realizar cadastro: ")
     print("2-Realizar login: ")
-    
-def mostrar_produtos():
+    print("3-Listar produtos: ")
+    print("4-Sair: ")
+
+def realizar_cadastro():
     pass
-    
+def sub_menu_cliente():
+    print("1-Visualizar produtos: ")
+    print("2-Adicionar produto ao carrinho: ")
+    print("3-Remover produto do carrinho: ")
+
+def realizar_login():
+    pass
+
 def listar_produtos():
     pass
+    
+# def sair():
+    
+    
+#     while True:
+#        menu_principal()
+#        op=int(input("Opções")) 
+#        if 1==op:
+#            realizar_cadastro
+#        if 2==op:
+#            realizar_login
+#        if 3==op:
+#            listar_produtos
+#        if 4==op:
+#            sair

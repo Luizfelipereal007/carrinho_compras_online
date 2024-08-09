852f8f8514a87ec84ba6839c36c18515e2a94793
Gui0403 <pabloguilhermemartinez@gmail.com>
2024-08-08 21:45:50 -0400

    functions 1

--- Function.py
+++ Function.py
@@ -1,6 +1,14 @@
 def Adicionar_produto():
-    pass
+    
+    
 
-def Exluir_produto():
+def Exluir_produto_carrinho():
     pass
     
+def excluir_carrinho():
+    posicao = 1
+    for produto in lista_produto:
+        if len(lista_produtos) > 0:
+            print(f"O Produto na posição {posicao}° é\nNome -->{produto.nome}\tId -->{produto.id}")
+            posicao += 1
+            

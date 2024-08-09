47ab7be3eb83abcfbece488a4f434735312ab2cc
Caio373 <fagundesc373@gmail.com>
2024-08-09 19:24:18 -0400

    Function-caio02.py

--- function-caio.py
+++ function-caio.py
@@ -1,18 +1,21 @@
+from Views import *
+from Classes import *
 def Adicionar_produto():
     nome = input("Nome do produto: ")
     preço = int(input("Digite o preço do produto: "))
     quantidade = int(input("Digite o preço do produto: "))
     descricao = input("Insira a descrição do produto: ")
-    produto = Produto(nome, preço, quantidade, descricao)
-    lista_produtos.append(produto)
+    id_produto = int(input("Digite o id do produto: "))
+    produto = Produto(nome, preço, quantidade,descricao, id_produto)
+    Loja.lista_produto.append(produto)
     
 
 def Excluir_produto():
     codigo = input('Digite o codigo do produto: ')
     i=0
-    for produto in lista_produtos:
+    for produto in listar_produtos:
         if codigo == produto.codigo:
-            lista_produtos.pop(i)
+            Loja.lista_produto.pop(i)
             print("O produto foi deletado")
         i+=1
     

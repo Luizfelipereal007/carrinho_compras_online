8d32b26ce4e19c4f494a93573e3983f1d627beb9
l2fm <l2fm2005@gmail.com>
2024-08-13 20:00:14 -0400

    mm

--- Views.py
+++ Views.py
@@ -2,8 +2,40 @@ from Function import*
 from Classes import*
 
 def menu_principal():
-    print("1-Criar usuario: ")
-    print("2-login usuario: ")
-    print("3-Adicionar produto: ")
-    print("4-Editar produto: ")
-    print("5-Remover produto: ")
+    print("1-Realizar cadastro: ")
+    print("2-Realizar login: ")
+    print("3-Listar produtos: ")
+    print("4-Criar usuario: ")
+    print("5-login usuario: ")
+    print("6-Adicionar produto: ")
+    print("7-Editar produto: ")
+    print("8-Remover produto: ")
+    print("9-Sair: ")
+
+def realizar_cadastro():
+    pass
+def sub_menu_cliente():
+    print("1-Visualizar produtos: ")
+    print("2-Adicionar produto ao carrinho: ")
+    print("3-Remover produto do carrinho: ")
+
+def sub_menu_adm():
+    print("1-Adicionar produto: ")
+    print("2-Excluir produto: ")
+
+def realizar_login():
+    pass
+def adicionar_produto():
+    pass
+
+def listar_produtos():
+    pass
+def excluir_produto():
+    pass
+def excluir_carrinho():
+    pass
+def criar_usuario():
+    pass
+
+def sair():
+    pass

79f85585533815d558c32236ca47a1f04a95cbee
Eduardo28110 <eduardofilho2811@gmail.com>
2024-08-13 21:36:02 -0400

    correto

--- Classes.py
+++ Classes.py
@@ -2,48 +2,55 @@
 class Loja:
     lista_produto = []
     lista_usuario = []
-    nome: True
+    nome: str
     validacao: bool
 
 
-    def __init__(self,nome,validacao):
+    def __init__(self,nome = None,validacao = None):
         nome = nome
         validacao = validacao
 
 class Produto():
-    id = str
+    id: str
     nome: str
     preco: float
     descricao: str
     codigo: str
 
-    def __init__(self,nome,preco,descricao,codigo):
-        nome = nome
-        preco = preco
-        descricao = descricao
-        codigo = codigo
+    def __init__(self, id = None, nome = None,preco = None,descricao = None,codigo = None):
+        self.id = id
+        self.nome = nome
+        self.preco = preco
+        self.descricao = descricao
+        self.codigo = codigo
 
 class Usuario():
-    id = str
+    id: str
     nome: str
     idade: float
     cpf: str
     telefone:float
-    email:str
+    email:str 
+    usuario: str
     senha:str
 
-    def __init__(self,id,nome,idade,cpf,telefone,email,senha):
+    def __init__(self,id = None,nome = None,idade = None,cpf = None,telefone = None,email = None,usuario = None,senha = None):
         self.id = id
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
         self.telefone = telefone
         self.email = email
+        self.usuario = usuario
         self.senha = senha    
     
 class Administrador(Usuario):
-    id = ''
+    usuario = 'ADM'
+    senha = "123"
 
-    def __init__(self,id):
+    def __init__(self, id= None, nome = None,usuario = None , senha = None):
+        super().__init__(id,nome,usuario, senha)
         self.id = id
-        self.id = ''
+        self.nome = nome
+        self.usuario = usuario
+        self.senha = senha
